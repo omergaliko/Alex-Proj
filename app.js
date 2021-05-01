1249ba940c91d0f6b67260d2987b1af1ebb11d9a
Omer Galiko <omergaliko22@gmail.com>
2021-05-01 14:36:17 +0300

    new text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('testing!@#@!$#@%$'));
+app.get('/', (req, res) => res.send('<h2>testing my first web app</h2></br><b>welcome to Omers pipline</b>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
